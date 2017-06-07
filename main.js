3a1a0139ae8405c8377cfdbccb32037b51689887
Kornelia Kobiela <korneliak.95@gmail.com>
2017-06-08 00:47:56 +0200

    Adding comments and change navigation

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 var capture, canvas, img;
-
+var photoButton, downloadButton;
+//elements which are made once during launching app
 function setup() {
   capture = createCapture(VIDEO);
-  capture.mouseClicked(getImage);
+  photoButton = createButton('Take a photo!');
+  photoButton.parent('controlPanel');
+  photoButton.mouseClicked(getImage);
   canvas = createCanvas(640,480);
+  downloadButton = createButton('Download a photo!');
+  downloadButton.parent('controlPanel');
+  downloadButton.mouseClicked(download);
 }
+
 
 function draw() {
   if (mouseIsPressed) {
@@ -12,12 +19,12 @@ function draw() {
     ellipse(mouseX, mouseY, 10, 10);
   }
 }
+//getting image from capture
 function getImage() {
   img = capture.get(0,0,width,height);
   image(img,0,0);
 }
-function keyPressed() {
-  if(keyCode === ENTER){
-    saveCanvas(canvas, 'portrait', 'jpg');
-  }
+
+function download() {
+    saveCanvas(canvas, 'portrait-' + random(1000), 'jpg');
 }
