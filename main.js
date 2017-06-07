09cd3ba9ffd9b308846f6cc2bba381f8db0d4d84
Kornelia Kobiela <korneliak.95@gmail.com>
2017-06-08 00:48:59 +0200

    add comments

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var capture, canvas, img;
-var photoButton, downloadButton;
+var capture, canvas, img, photoButton, downloadButton;
 //elements which are made once during launching app
+
 function setup() {
   capture = createCapture(VIDEO);
   photoButton = createButton('Take a photo!');
@@ -12,19 +12,21 @@ function setup() {
   downloadButton.mouseClicked(download);
 }
 
-
+//drawing shapes with mouse
 function draw() {
   if (mouseIsPressed) {
     fill(0);
     ellipse(mouseX, mouseY, 10, 10);
   }
 }
+
 //getting image from capture
 function getImage() {
   img = capture.get(0,0,width,height);
   image(img,0,0);
 }
 
+//download image to disk
 function download() {
     saveCanvas(canvas, 'portrait-' + random(1000), 'jpg');
 }
